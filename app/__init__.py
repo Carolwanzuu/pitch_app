0496b7faae92b5837009f7b47760d9f6f355dd14
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 06:58:07 +0300

    importing and creating a db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
-
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,7 +13,8 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
 # initializing flask extensions
-    bootstrap = Bootstrap()
+    bootstrap.init_app(app)
+    db.init_app(app)
 
 # registering a blueprint
     from .main import main as main_blueprint
