8ae7a5ce23baf31a3a0518ef3425db057845c827
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 05:58:27 +0300

    installing and initializing flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__)
 
 # setting up configurations
 app.config.from_object(DevConfig)
+
+# initializing flask extensions
+bootstrap = Bootstrap()
 
 from app import views
