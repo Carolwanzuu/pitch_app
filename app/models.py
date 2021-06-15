b607ff2a9408aca6c9c5dbd83f1e473dd5bff112
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 06:29:55 +0300

    changing the folder structure

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 class User():
-    def __init__(self,id,title,overview):
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
         self.id =id
         self.title = title
         self.overview = overview
