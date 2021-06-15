4ecd9d271e331901dfc54f490c17fa8cdf09f54d
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 09:36:21 +0300

    creating a class for pitch

--- app/models.py
+++ app/models.py
@@ -8,3 +8,16 @@ class User(db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    pitch_content = db.Column(db.String)
+    category = db.Column(db.String(255))
+    author = db.Column(db.String(255))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)        
+    published_at = db.Column(db.DateTime, default = datetime.utcnow)    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref = 'pitch', lazy = 'dynamic')
