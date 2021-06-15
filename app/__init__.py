c4c473420f785aa9eb6e4e5094658edf33449ee7
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 15:20:36 +0300

    creating a configuration for uploading photos

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -10,12 +11,15 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
+# configure UploadSet
+    configure_uploads(app,photos)
 
 # setting up configurations
     app.config.from_object(config_options[config_name])
