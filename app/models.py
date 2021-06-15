6f03d3a0ad65211e00ee8373c068573b35081b22
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 08:46:19 +0300

    passing db.Model to our class user to all db communications

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
-class User():
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+:
+    
