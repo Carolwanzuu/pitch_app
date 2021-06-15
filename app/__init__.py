e63c38b95c2d5a257b6aa801cd720dd802bfdf97
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 06:37:10 +0300

    updating  our app/__init__.py to create our application factory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
-# Initializing application
-app = Flask(__name__)
 
-# setting up configurations
-app.config.from_object(DevConfig)
-
-# initializing flask extensions
 bootstrap = Bootstrap()
 
-from app import views
-from app import error
+def create_app(config_name):
+
+# Initializing application
+    app = Flask(__name__)
+
+# setting up configurations
+    app.config.from_object(config_options[config_name])
+
+# initializing flask extensions
+    bootstrap = Bootstrap()
+
+    return app
+
+
