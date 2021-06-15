f7584e5972f5035dbc10980f83585b1b7977a7c3
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-15 17:49:38 +0300

    editing the main/views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,25 +1,35 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from flask_login import login_required
 from ..models import User
-from .forms import  UpdateProfile
+from .forms import  PitchForm,UpdateProfile,CommentsForm
+from flask_login import login_required, current_user
 from .. import db, photos
 
 # Views
 @main.route('/')
+@login_required
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'Pitches'
+    general = Pitch.query.all()
+    product_pitch = Pitch.query.filter_by(category = 'Product Pitch').all()
+    pickup_lines = Pitch.query.filter_by(category = 'Pickup Lines').all()
+    interview_pitch = Pitch.query.filter_by(category = 'Interview Pitch').all()
+    promotion_pitch = Pitch.query.filter_by(category = 'Promotion Pitch').all()
 
-    return render_template('index.html')
+
+    return render_template('index.html', title = title,general = general,product_pitch = product_pitch,pickup_lines = pickup_lines,interview_pitch = interview_pitch,promotion_pitch = promotion_pitch)
+
 @main.route('/user/<uname>')
+@login_required
 def profile(uname):
     User = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
-    return render_template('profile.html',user = user)
+    return render_template('profile/profile.html',user = user)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
